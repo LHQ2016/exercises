ed87418b3f91f7cd6a22f9e91b6fb50db6ab54ae
ChenZi <2016LHQ@gmail.com>
2018-05-23 23:15:39 +0800

    修改文件路径

--- src/io/copy/CopyData06.java
+++ src/io/copy/CopyData06.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class CopyData06 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(".\\src\\resource\\txt\\bbb.txt"));
-        BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\李华强\\Desktop\\test.txt"));
+        BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\李华强\\test.txt"));
 
         int len = 0;
         char[] chs = new char[1024];
--- src/io/copy/CopyDate04.java
+++ src/io/copy/CopyDate04.java
@@ -10,7 +10,7 @@ import java.io.*;
 public class CopyDate04 {
     public static void main(String[] args) throws IOException {
         OutputStreamWriter outputStreamWriter = new OutputStreamWriter
-                (new FileOutputStream("C:\\Users\\李华强\\Desktop\\test.txt"));
+                (new FileOutputStream("C:\\Users\\李华强\\test.txt"));
         InputStreamReader inputStreamReader = new InputStreamReader
                 (new FileInputStream(".\\src\\resource\\txt\\红楼梦.txt"));
 
